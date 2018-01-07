2a6997c45d55db142806b3d912e747d876e1504e
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-07 21:22:03 +0100

    Hello React

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+/*var template = <p>This is JSX from app.js!</p>;*/
+
+var template = React.createElement(
+    "h1",
+    { id: "someid" },
+    "Something new"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
