8da2575152ffb615dec3381eea82fd5e5d035093
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-09 17:38:13 +0100

    ES6 aside: arrow function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,81 +1,13 @@
 'use strict';
 
-console.log('App.js is running!');
-
-// JSX - JavaScript XML
-var app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+/*
+const getFirstName = (fullName) => {
+  return fullName.split(' ')[0];
 };
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
+*/
 
-var user = {
-    name: 'Inioluwa',
-    age: 20,
-    location: 'Lagos'
+var getFirstName = function getFirstName(fullName) {
+  return fullName.split(' ')[0];
 };
 
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            user.location
-        );
-    }
-}
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log(getFirstName('Mike Smith'));
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,10 @@
+/*
+const getFirstName = (fullName) => {
+  return fullName.split(' ')[0];
+};
+*/
+
+
+const getFirstName = (fullName) => fullName.split(' ')[0];
+
+console.log(getFirstName('Mike Smith'));
