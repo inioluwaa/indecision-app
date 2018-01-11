67b2564b44afed23c9fc6e508f91983c24395f7e
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-11 11:28:58 +0100

    use event binding, forms and inputs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,88 @@
-"use strict";
+'use strict';
 
-var multiplier = {
-  numbers: [2, 5, 10],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    var _this = this;
+console.log('App.js is running!');
 
-    return this.numbers.map(function (number) {
-      return number * _this.multiplyBy;
-    });
+// JSX - JavaScript XML
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
   }
 };
 
-console.log(multiplier.multiply());
+var onremoveAll = function onremoveAll() {
+  app.options = [];
+  render();
+};
+
+var render = function render() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'button',
+      { onClick: onremoveAll },
+      'Remove all'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'Item one'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item two'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add option'
+      )
+    )
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+var appRoot = document.getElementById('app');
+
+render();
